52a6c4d5065d20560fb7cbd5868292ed1b4dd812
shahin <shahin.parveen@ejamerica.com>
2022-08-01 16:42:33 +0530

    first commit

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB'>
+          <div className='logo'>
+            <h1>LIYO</h1>
+            <span>LIYO--WEB DEVELOPMENT SITE--LEARN IT YOURSELF FROM ONLINE</span>
+          </div>
+
+          <div className='social'>
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-instagram icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO LIYO-WEB-DEVELOPMENT-PLATFORM' title='Best Online Web Developer Expertise Are Here..' />
+            <p> A successful website does three things: It attracts the right kinds of visitors. Guides them to the main services or products you offer. Collect Contact details for future ongoing relation.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW PROJECTS <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
